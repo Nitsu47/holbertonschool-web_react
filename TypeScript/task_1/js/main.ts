24af80c329c13a5e84fd44a5ebf2361260f9eae2
Nitsu47 <agustraversa2004@gmail.com>
2024-08-09 04:47:41 -0300

    Student class with constructor, methods and interfaces

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -18,3 +18,30 @@ export interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
 	return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+class StudentClass {
+	firstName: string;
+	lastName: string;
+
+constructor(firstName: string, lastName: string) {
+	this.firstName = firstName
+	this.lastName = lastName
+}
+
+workOnHomework(): string {
+	return 'Currently working';
+}
+
+displayName(): string {
+	return `${this.firstName}`
+}
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
